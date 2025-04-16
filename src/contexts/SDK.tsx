729052233cd3451de656cc26962ee03bbb26219a
alexandraschlueter <alexandra.schlueter@adesso.de>
2025-04-16 16:30:11 +0200

    use sdk in context instead of globalthis

---
+++ src/contexts/SDK.tsx
@@ -0,0 +1,57 @@
+import React, {createContext, ReactNode, useContext, useEffect, useState} from 'react'
+import {IResponseDeserializer, SpotifyApi} from "@spotify/web-api-ts-sdk";
+import {Scopes} from "@/scopes";
+
+interface SDKValidation {
+    sdk: SpotifyApi | undefined;
+
+}
+
+const spotifyClientId = process.env.NEXT_PUBLIC_AUTH_SPOTIFY_ID!;
+
+export default class FixedResponseDeserializer
+    implements IResponseDeserializer {
+    public async deserialize<TReturnType>(
+        response: Response,
+    ): Promise<TReturnType> {
+        const text = await response.text();
+
+        const contentType = response.headers.get("content-type") ?? '';
+
+        if (text.length > 0 && contentType.includes("application/json")) {
+            const json = JSON.parse(text);
+            return json as TReturnType;
+        }
+
+        return null as TReturnType;
+    }
+}
+
+const SDKContext = createContext<SDKValidation>({} as SDKValidation)
+export const SDKProvider = ({children}: { children: ReactNode | ReactNode[] }) => {
+    const [sdk, setSDK] = useState<SpotifyApi>();
+
+    async function setUpAuth() {
+        console.log('setting sdk now in context');
+        setSDK(SpotifyApi.withUserAuthorization(spotifyClientId, "http://localhost:3000/home", Scopes.all, {deserializer: new FixedResponseDeserializer()}));
+        await sdk?.authenticate().then((authRes) => console.log(authRes));
+    }
+
+    useEffect(() => {
+        setUpAuth();
+    }, []);
+
+    return (
+        <SDKContext.Provider
+            value={{
+                sdk
+            }}
+        >
+            {children}
+        </SDKContext.Provider>
+    )
+}
+
+export function useSDK(): SDKValidation {
+    return useContext(SDKContext)
+}
